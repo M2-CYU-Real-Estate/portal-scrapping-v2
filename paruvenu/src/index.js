c9ebe06545328e18a56763d28fd604cd2ed013e3
AldricVS <aldric.silvestre@outlook.fr>
2023-04-12 14:28:23 +0200

    Fix performance for paruvendu

--- paruvenu/src/index.js
+++ paruvenu/src/index.js
@@ -1,39 +1,46 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 const commandLineArgs = require('command-line-args');
-const { title } = require('process');
+
+const DELAY_AFTER_LOAD_MS = 350;
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 async function scrapeData(url) {
   const data = [];
   const browser = await puppeteer.launch({
-      args: ['--no-sandbox'], // useful when using docker (allow using app as admin)
-      headless: false,
-      ignoreHTTPSErrors: true,
-    });
-  const page = await browser.newPage();
+    args: ['--no-sandbox'], // useful when using docker (allow using app as admin)
+    headless: false,
+    ignoreHTTPSErrors: true,
+  });
 
-  // Set a timeout for all subsequent actions performed on the page
-  page.setDefaultTimeout(50000); // 30 seconds
+  try {
+    const page = await (await browser.pages()).at(0);
+    if (!page) {
+      throw new Error("page is not opened properly");
+    }
 
-  try{
-    await page.goto(url);
-
-    // Wait for the button to appear
-    await page.waitForSelector('div.container.buttons button:nth-child(2)');
+    // Set a timeout for all subsequent actions performed on the page
+    page.setDefaultTimeout(50000); // 30 seconds
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await delay(DELAY_AFTER_LOAD_MS);
 
     // Click on the button
     await page.click('div.container.buttons button:nth-child(2)');
-
-    // Wait for the button to disappear
-    await page.waitForSelector('div.container.buttons button:nth-child(2)', { hidden: true });
 
     //Scrape the data and push it into the `data` array
 
     const hrefs = await page.$$eval('.flex.sm\\:block.gap-4 a', links => links.map(link => link.href));
 
-    let i=1;
+    let i = 1;
     for (const href of hrefs) {
-      await page.goto(href);
+      await page.goto(href, { waitUntil: 'domcontentloaded' });
+      await delay(DELAY_AFTER_LOAD_MS);
       const pageTitle = await page.title();
       // Remove all special characters
       const cleanInput = pageTitle.replace(/[^\w\s]/gi, '');
@@ -87,14 +94,14 @@ async function scrapeData(url) {
             const [key, value] = span.textContent.trim().split(':');
             if (key && value) {
               result[key.trim()] = value.trim();
-            }else{
-              result[key.trim()] =null;
+            } else {
+              result[key.trim()] = null;
             }
           });
         }
         return result;
       });
-      
+
       const imageSelector = 'img.im11_pic_main';
       const imageElement = await page.$(imageSelector);
       const image = imageElement ? await imageElement.evaluate(el => el.src) : 'none';
@@ -114,29 +121,28 @@ async function scrapeData(url) {
 
       const descriptionSelector = 'div#txtAnnonceTrunc';
       const descriptionElement = await page.$(descriptionSelector);
-      const description = descriptionElement ? await descriptionElement.evaluate(el => el.textContent.replace(/[\n\t]/g, '').replace(/\s{2,}/g, ' '))  : 'none';
-      
+      const description = descriptionElement ? await descriptionElement.evaluate(el => el.textContent.replace(/[\n\t]/g, '').replace(/\s{2,}/g, ' ')) : 'none';
+
       const hasCuisine = description.includes('cuisine');
       const cuisine = hasCuisine ? true : false;
 
-      const scrapedData = { title: pageTitle,url,ref,typeBien,ville,codePostale, price, surface, pieces,cuisine, features, description, image };
+      const scrapedData = { title: pageTitle, url, ref, typeBien, ville, codePostale, price, surface, pieces, cuisine, features, description, image };
       data.push(scrapedData);
       console.log(`[ANNONCE ${i} SCRAPPED]`);
-      i +=1;
+      i += 1;
 
     }
-  }catch (e) {
+  } catch (e) {
     if (e instanceof puppeteer.errors.TimeoutError) {
       console.error('Timeout error:', e.message);
-      await browser.close();
       return 1;
-    }else {
+    } else {
       console.error('Other error:', e.message);
     }
+  } finally {
+    await browser.close();
   }
 
-
-  await browser.close();
   return data;
 }
 
@@ -147,19 +153,19 @@ async function run(url) {
 
 async function scrapeAllPages() {
   const allData = [];
-  let i =1;
-  while(true && i <=300){
-    
+  let i = 1;
+  while (true && i <= 10) {
+
     const url = `https://www.paruvendu.fr/immobilier/vente/maison/?p=${i}`;
     const data = await run(url);
-    
+
     if (data === undefined || data.length == 0) {
       console.log("[SCRAPPING HAS FINISHED]");
       break;
-    }else if (data == 1){
-      i+=1;
-     continue;
-    }else{
+    } else if (data == 1) {
+      i += 1;
+      continue;
+    } else {
       allData.push(...data);
       console.log("[SCRAPPING FINISH FOR PAGE]", i)
       i++;
@@ -170,28 +176,27 @@ async function scrapeAllPages() {
 
 const parseArgs = () => {
   const optionDefitions = [
-      {
-          // Required
-          name: "output",
-          alias: "o",
-          type: String 
-      }
+    {
+      // Required
+      name: "output",
+      alias: "o",
+      type: String
+    }
   ];
   const options = commandLineArgs(optionDefitions);
 
   // Check if valid
   if (!options.output) {
-      throw new Error('"--output" argument required');
+    throw new Error('"--output" argument required');
   }
-  if (!fs.existsSync(options.output)) {
-      throw new Error(`Output directory does not exist : ${options.output}`);
-  }
-  
+
   return options;
 };
 
 
 const main = async () => {
+
+  const startTime = Date.now();
 
   const args = parseArgs();
   const outputPath = args.output;
@@ -199,11 +204,15 @@ const main = async () => {
 
   //TODO, call the specific method(s) for fetching data (iterable, array of jsons, whatever)
   scrapeAllPages()
-      .then(data => {
-          fs.writeFileSync(outputPath,
-          // TODO, use outputPath to save to specific path
-          JSON.stringify(data)
-          )});
+    .then(data => {
+      fs.writeFileSync(outputPath,
+        // TODO, use outputPath to save to specific path
+        JSON.stringify(data)
+      )
+    });
+
+  const endTime = Date.now();
+  console.log(`[TOTAL EXECUTION TIME : ${endTime - startTime}ms]`);
 };
 
 
